38cbd349e67c71688cb4459767f684e45aac4e5e
Steffen Karcher <stekarcher@gmail.com>
2024-08-17 22:52:57 +0200

    recursive generating done

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ def main():
     #node = TextNode("This is a text node", "bold", "https://www.boot.dev")
     #print(node)
     copy_recursive(os.path.abspath(os.getcwd()+"/static"), os.path.abspath(os.getcwd())+"/public")
-    generate_page(os.path.abspath(os.getcwd()+"/content/index.md"),os.path.abspath(os.getcwd()+"/template.html"),os.path.abspath(os.getcwd()+"/public/index.html"))
+    generate_pages_recursive(os.path.abspath(os.getcwd()+"/content"),os.path.abspath(os.getcwd()+"/template.html"),os.path.abspath(os.getcwd()+"/public"))
 
 def copy_recursive(source, destination):
     if os.path.exists(destination):
@@ -26,8 +26,15 @@ def copy_recursive(source, destination):
             copy_recursive(item,destination+"/"+os.path.split(copy)[1]) 
 
 def generate_pages_recursive(dir_path_content,template_path, dest_dir_path):
+    list_of_files = os.listdir(dir_path_content)
+    for file in list_of_files:
+        filepath = f"{dir_path_content}/{file}"
+        if os.path.isfile(filepath):
+            generate_page(filepath,template_path,dest_dir_path+"/"+file)
+        if os.path.isdir(filepath):
+            os.mkdir(dest_dir_path+"/"+file)
+            generate_pages_recursive(filepath,template_path,dest_dir_path+"/"+file)    
     
-    pass
 
 def generate_page(from_path,template_path, dest_path):
     print(f"Generating page from path {from_path} to {dest_path} using {template_path}.")
@@ -39,6 +46,8 @@ def generate_page(from_path,template_path, dest_path):
     template = template.replace("{{ Content }}",convert)
     if not os.path.isdir(os.path.split(dest_path)[0]):
         os.path.makedir(os.path.split(dest_path)[0])
+    dest_path = dest_path.rstrip(".md")
+    dest_path = f"{dest_path}.html"
     htmlfile = open(dest_path,"w")
     htmlfile.write(template)
     htmlfile.close()
